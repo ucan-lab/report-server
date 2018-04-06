5eab6393934e8d0ea9b9a4235085a171331fdc01
2ndwave.imamura <35098175+ucan-lab@users.noreply.github.com>
2018-04-06 21:52:16 +0900

    docker test

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$mysqli = new mysqli('db', 'root', 'root', 'app');
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+echo 'Success... ' . $mysqli->host_info . "\n";
+$mysqli->close();
+
