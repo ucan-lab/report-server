39f2361b30edd1f3bcd8106b57b6f4e309b3597c
y.imamura <35098175+ucan-lab@users.noreply.github.com>
2018-04-08 15:18:46 +0900

    docker laravel

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli('db', 'root', 'root', 'app');
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-}
-echo 'Success... ' . $mysqli->host_info . "\n";
-$mysqli->close();
-
